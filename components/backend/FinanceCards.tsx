d322e38587b35b52723bb43b97d7838a05749b80
Ubaton <raymondng729@gmail.com>
2024-05-11 08:46:02 +0200

    FIxing the hydration error of the HTML

--- components/backend/FinanceCards.tsx
+++ components/backend/FinanceCards.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import FinanceCard from './FinanceCard'
+import React from "react";
+import FinanceCard from "./FinanceCard";
 
 export default function FinanceCards() {
   const orderStats = [
@@ -18,22 +18,17 @@ export default function FinanceCards() {
     {
       title: "All-Time Sales",
       sales: 135350,
-    }
-]
+    },
+  ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2
-    md:grid-cols-3 lg:grid-cols-4 gap-4 py-6">
-
-      {
-        orderStats.map((item,i)=>{
-          return(
-            <FinanceCard data={item} key={i}/>
-          )
-        })
-      }
-
-
+    <div
+      className="grid grid-cols-1 sm:grid-cols-2
+    md:grid-cols-3 lg:grid-cols-4 gap-4 py-6"
+    >
+      {orderStats.map((item, index) => {
+        return <FinanceCard data={item} key={index} />;
+      })}
     </div>
-  )
+  );
 }
