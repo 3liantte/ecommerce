89dd9b9247b86551fd28c4f431a3f907aa81d074
Ubaton <raymondng729@gmail.com>
2024-05-13 07:45:57 +0200

    Navbar and the ThemeSwitch Tailwind CSS changes

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -5,22 +5,23 @@ import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
 
 export default function ThemeSwitcher() {
-const [mounted, setMounted] = useState(false);
-const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-useEffect(() => {
-  setMounted(true);
-}, []);
-if (!mounted) {
-return null;
-}
-return (
-  <button
-    className={`relative inline-flex items-center p-4 font-medium 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+  return (
+    <button
+      className={`relative inline-flex items-center p-4 font-medium 
     text-center text-indigo-900 dark:text-yellow-500 bg-transparent rounded-lg hover:bg-green-300  
     dark:hover:bg-green-700`}
-    onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
       {theme === "light" ? <Moon /> : <Sun />}
-  </button>
-);
+    </button>
+  );
 }
