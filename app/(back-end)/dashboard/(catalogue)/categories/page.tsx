6f21a596771809175134769dc84dede73ea169d4
Ubaton <raymondng729@gmail.com>
2024-05-20 23:06:54 +0200

    Category List with the "Get" function from the API to get DATA to be rendered.

--- app/(back-end)/dashboard/(catalogue)/categories/page.tsx
+++ app/(back-end)/dashboard/(catalogue)/categories/page.tsx
@@ -1,21 +1,28 @@
-import Heading from '@/components/backend/Heading/Heading'
-import PageHeader from '@/components/backend/Heading/PageHeader'
-import TableActions from '@/components/backend/FormInputs/TableActions'
-import { Download, Plus, Search, Trash, Trash2 } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+import Heading from "@/components/backend/Heading/Heading";
+import PageHeader from "@/components/backend/Heading/PageHeader";
+import TableActions from "@/components/backend/FormInputs/TableActions";
+import { Download, Plus, Search, Trash, Trash2 } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import TableList from "@/components/backend/NewCategriesTable/TableList";
 
-export default function page(){
+export default function page() {
   return (
-    <div>
-      {/* Header */}
-        <PageHeader heading="Categories" href="/dashboard/categories/new" 
-        linkTitle="Add Category"/>
+    <>
+      <PageHeader
+        heading="Categories"
+        href="/dashboard/categories/new"
+        linkTitle="Add Category"
+      />
 
-      {/* Table (Actions) */}
-      <TableActions/>
-      
-      <h2>Table</h2>
-    </div>
-  )
+      <div>
+        <TableActions />
+      </div>
+
+      <div className="flex flex-col pt-6">
+        <h1 className="text-3xl font-bold pt-4 pb-4">Categories List</h1>
+        <TableList />
+      </div>
+    </>
+  );
 }
--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,8 +1,11 @@
 import { createRouteHandler } from "uploadthing/next";
- 
+
 import { ourFileRouter } from "./core";
- 
+
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: {
+    callbackUrl: `http://localhost:${process.env.PORT ?? 3000}/api/uploadthing`,
+  },
 });
